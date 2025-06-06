a73caacfd2916fbd8e3f25b50c68a071e4febb92
Guilherme0Medeiros <guilherme272004@gmail.com>
2025-06-06 16:17:49 -0300

    tentativa de resolver erro de deploy

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -1,6 +1,7 @@
 import ProductDetail from "@/components/ProductDetail";
 
-export default async function ProductPage({ params }: { params: { id: string } }) {
+export default async function ProductPage(props: { params: Promise<{ id: string }> }) {
+  const params = await props.params;
   const res = await fetch(`https://fakestoreapi.com/products/${params.id}`);
   if (!res.ok) {
     return <div>Produto não encontrado</div>;
