cc8d1494fe8c667a5c21f9ceb26b1b0393bcb43d
Guilherme0Medeiros <guilherme272004@gmail.com>
2025-06-06 15:16:52 -0300

    Corrigindo mais erros

--- src/app/produtos/[id]/page.tsx
+++ src/app/produtos/[id]/page.tsx
@@ -1,14 +1,15 @@
-import ProductDetail from "@/components/ProductDetail"
+import ProductDetail from "@/components/ProductDetail";
 
-// Função server-side (App Router)
 export default async function ProductPage({ params }: { params: { id: string } }) {
-  const res = await fetch(`https://fakestoreapi.com/products/${params.id}`)
-  if (!res.ok) return <div>Produto não encontrado</div>
-  const product = await res.json()
+  const res = await fetch(`https://fakestoreapi.com/products/${params.id}`);
+  if (!res.ok) {
+    return <div>Produto não encontrado</div>;
+  }
+  const product = await res.json();
 
   return (
     <main className="flex justify-center items-center min-h-[80vh] bg-background py-8">
       <ProductDetail product={product} />
     </main>
-  )
+  );
 }
